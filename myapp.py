bb854b661d51ba4f2907b4775c4bd5c01a643bec
viveknikam1 <47035774+viveknikam1@users.noreply.github.com>
2020-06-12 23:23:56 +0530

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -18,7 +18,7 @@ def get_result():
     X_query = poly.transform(query)
     sal = model.predict(X_query)
     # use model.predict to predict salary
-    return 'Dear '+request.form["text1"]+ ' Your predicted salary after ' + request.form["text2"] + ' Experience is :'+ str(sal)
+    return 'Dear '+request.form["text1"]+ ' Your predicted salary after ' + request.form["text2"] + ' Experience is :'+ str(sal) + 'LPA modified'
 
 if __name__=='__main__':
     app.run(debug=True)
